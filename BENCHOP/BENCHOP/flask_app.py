d54859faee3cdc433486af1c0887423501728bf1
banan1994 <patrik.bjorklund1@gmail.com>
2019-10-30 21:21:47 +0100

    run some problems and return whatever is finished

--- BENCHOP/BENCHOP/flask_app.py
+++ BENCHOP/BENCHOP/flask_app.py
@@ -1,26 +1,34 @@
-from flask import Flask
+from flask import Flask, jsonify
 from tasks import singleMethod
 import os
 from celery_app import celery_app
 from celery.result import AsyncResult
 
-TASK_ID = 0
+
+PROBLEMS = ['P1aI', 'P1bI', 'P1cI', 'P1bII']
+METHODS = ['COS']
+
+ALL_RESULTS = {}
+
+for prob in PROBLEMS:
+	ALL_RESULTS[prob] = {}
+	for method in METHODS:
+		result = singleMethod.delay(prob, method)
+		ALL_RESULTS[prob][method] = {'state': 'PENDING', 'id': result.id, 'result': ''}
 
 flask_app = Flask(__name__)
+
 @flask_app.route('/test', methods=['GET'])
 def test():
-	global TASK_ID
-	if TASK_ID == 0:
-		result = singleMethod.delay('P1bII', 'UniformGrid')
-		TASK_ID = result.id
-		return str(TASK_ID)
-	else:
-		result = AsyncResult(TASK_ID, app=celery_app)
-		state = result.state
-		if state == 'PENDING':
-			return state
-		else:
-			return str(result.result)
+	global ALL_RESULTS
+	for prob in ALL_RESULTS.keys()
+		for method in prob.keys():
+			if method['state'] == 'PENDING':
+				result = AsyncResult(method['id'], app=celery_app)
+				if result.state != 'PENDING':
+					method['state'] = result.state
+					method['result'] = result.result
+	return jsonify(ALL_RESULTS)
 
 if __name__ == '__main__':
 	flask_app.run(host='0.0.0.0', port=4567, debug=True)
